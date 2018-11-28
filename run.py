81b82648e25fcd5d3f04852ca034221074737bc1
Matthew Thomas <mdthomas@hotmail.ca>
2018-11-27 21:08:04 -0800

    Added message processing from client.

--- run.py
+++ run.py
@@ -3,8 +3,14 @@ import asyncio
 import os
 import secrets
 from collections import deque
+import json
+import logging
 
 
+logging.basicConfig(
+    format='%(asctime)s: %(levelname)s: %(module)s/%(funcName)s: %(message)s',
+    level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 HOST = '127.0.0.1'
 PORT = 2018
 
@@ -18,10 +24,10 @@ def main():
 
     start_server_task = loop.create_task(start_listening())
     task1 = loop.create_task(message_worker())
-    task2 = loop.create_task(connected_clients_worker(1))
-    task3 = loop.create_task(connected_clients_worker(2))
+    #task2 = loop.create_task(connected_clients_worker(1))
+    #task3 = loop.create_task(connected_clients_worker(2))
 
-    all_tasks = asyncio.gather(start_server_task, task1, task2, task3)
+    all_tasks = asyncio.gather(start_server_task, task1)
     loop.run_until_complete(all_tasks)
     #asyncio.run(start_listening(), debug=True)
 
@@ -30,8 +36,7 @@ async def start_listening():
     global server
     server = await asyncio.start_server(client_connected, HOST, PORT)
     addr = server.sockets[0].getsockname()
-    print(f'Server started on {addr} with PID {os.getpid()}')
-
+    logger.info(f'Server started on {addr[0]}:{addr[1]} with PID {os.getpid()}')
     async with server:
         await server.serve_forever()
 
@@ -40,20 +45,51 @@ async def client_connected(reader, writer):
     addr = writer.get_extra_info('peername')
     global connected_clients
     connected_clients.append(writer)
-    print(f'Connected clients {len(connected_clients)}.')
-    print(f'Client connected with address {addr}')
-    print('Sending msg.')
+    logger.info(f'Connected clients {len(connected_clients)}.')
+    logger.info(f'Client connected from {addr[0]}:{addr[1]}')
+    logger.info('Sending msg.')
     writer.write('Thanks for connecting.'.encode())
 
     data = None
     while data is not b'':
-        data = await reader.read(100)
-        message = data.decode()
-        print(f'Message received! {message}, From {addr}')
+        data = await reader.read(4096)
+        logger.info(f'*** {len(data)} bytes received from {addr[0]}:{addr[1]} ***')
+        process_data(data)
     
-    print('Client disconnected.')
+    logger.info(f'Client {addr[0]}:{addr[1]} disconnected.')
     connected_clients.remove(writer)
     writer.close()
+
+
+def process_data(data):
+    #logger.debug(f'Processing {len(data)} bytes.')
+    #logger.debug(f'The data: {data.decode()}')
+    data_len = len(data)
+    processed_len = 0
+    fixed_len = 2
+
+    while processed_len < data_len:
+        header_len = int.from_bytes(data[processed_len: processed_len + fixed_len], byteorder='big')
+        logger.debug(f'Processed header length: {header_len}')
+
+        header_start = processed_len + fixed_len
+        header_end = processed_len + fixed_len + header_len
+        header = unpack_data(data[header_start:header_end])
+        logger.debug(f'Processed header: {header}')
+
+        message_start = processed_len + fixed_len + header_len
+        message_end = processed_len + fixed_len + header_len + header['message_length']
+        # Check the first 2 bytes for the length(int) of the message header.
+        message = unpack_data(data[message_start:message_end])
+        logger.debug(f'Processed message: {message}')
+
+        processed_len += fixed_len + header_len + header['message_length']
+        logger.debug(f'Processed {processed_len}/{data_len} bytes. Message: {message}')
+
+
+def unpack_data(data):
+    json_header = data.decode()
+    return json.loads(json_header)
 
 
 async def connected_clients_worker(worker):
